4e59272f7ef0427a0e17389f55b121a66c97d924
Schilling Chiu <schillingc@muneris.io>
2017-06-06 15:26:44 +0800

    add main page, move navigator drawer to second view

--- iOSPractice/AppDelegate.swift
+++ iOSPractice/AppDelegate.swift
@@ -17,7 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        
+        let handled = true
+        
+        return handled
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
---
+++ iOSPractice/Classes/EnterViewController.swift
@@ -0,0 +1,63 @@
+//
+//  EnterViewController.swift
+//  iOSPractice
+//
+//  Created by Schilling Chiu on 2017/6/6.
+//  Copyright © 2017 honorwealthy. All rights reserved.
+//
+
+import UIKit
+
+class EnterViewController: UIViewController {
+    
+    @IBOutlet weak var menuTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        menuTableView.dataSource = self
+        menuTableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+// MARK: - UITableViewDataSource
+
+extension EnterViewController : UITableViewDataSource {
+    
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let reusableId = "functionCell"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: reusableId, for: indexPath)
+        
+        cell.textLabel?.text = "drawer test"
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension EnterViewController : UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let otherStoryboard = UIStoryboard(name: "NavigatorDrawer", bundle: nil)
+        let navigatorDrawer = otherStoryboard.instantiateViewController(withIdentifier: "NavigatorDrawer") as! KYDrawerController
+        self.present(navigatorDrawer, animated: true, completion: nil)
+    }
+}
