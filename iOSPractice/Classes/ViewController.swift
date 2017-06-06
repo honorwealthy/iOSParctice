b741fc14117a9ea239cafe2f230d5c675c801238
Schilling Chiu <schillingc@muneris.io>
2017-06-06 10:41:24 +0800

    init commit

---
+++ iOSPractice/Classes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSPractice
+//
+//  Created by Schilling Chiu on 2017/6/6.
+//  Copyright © 2017 honorwealthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
